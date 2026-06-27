7a3d817360239e4b15e0b58a136ca7c918ba491b
koala-man-64 <rdprokes@gmail.com>
2026-06-27 14:50:50 -0500

    TCU-VERTICAL-SLICE fix: align Android JVM target

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -32,6 +32,15 @@ android {
         buildConfig = true
         compose = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
